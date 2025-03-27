c61d881c86f60463e406ab9d4b302060d782ff59
Krishna-wable <krishnawable11@gmail.com>
2025-03-27 15:49:33 +0530

    paint

--- src/components/guide.tsx
+++ src/components/guide.tsx
@@ -22,7 +22,7 @@ const Guide: React.FC = () => {
   ];
 
   return (
-    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-20 py-8 md:py-12 bg-gray-100 gap-8 md:gap-16 justify-center items-center">
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-10 py-8 md:py-12 bg-gray-100 gap-8 md:gap-16 justify-center items-center">
       {/* Left Section */}
       <div className="w-full md:w-1/2 space-y-6 md:space-y-8 pr-0 md:pr-8 lg:pr-16 text-center md:text-left">
         <button className="text-lg bg-gray-300 text-black px-4 py-3 rounded-lg">Colour Guide</button>
@@ -37,7 +37,7 @@ const Guide: React.FC = () => {
               <Image
                 src={`/assets/house/${room.img}`}
                 alt={room.text}
-                width={112} // Adjust sizes as per your design
+                width={112} // Adjust size accordingly
                 height={136}
                 className="w-20 h-24 md:w-24 md:h-28 lg:w-28 lg:h-32 rounded-lg cursor-pointer"
                 onClick={() => {
@@ -69,7 +69,7 @@ const Guide: React.FC = () => {
           <Image
             src={`/assets/house/${selectedRoom}`}
             alt="Selected Room"
-            width={640} // Adjust sizes
+            width={400} // Adjust size accordingly
             height={320}
             className="w-full h-full object-cover rounded-lg"
           />
