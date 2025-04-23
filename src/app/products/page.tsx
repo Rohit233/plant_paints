7e4f1dc64e0d2c8ceec234f004ee84add995afbc
Krishna-wable <krishnawable11@gmail.com>
2025-04-23 22:31:08 +0530

    code

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,16 +3,25 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import Product from "@/components/product";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Products() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
   return (
     <main className="w-full min-h-screen bg-gray-50">
       <Navbar />
       
-      <Product id={id}/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductContent />
+      </Suspense>
+      
       <Footer />
     </main>
   );
+}
+
+function ProductContent() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  
+  return <Product id={id} />;
 }
