7ae5f53a7493d98d0e9ba26837f25d3d65196396
Krishna-wable <krishnawable11@gmail.com>
2025-04-24 18:38:13 +0530

    website

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "cdn.pixabay.com", "images.pexels.com"],
+    domains: ["images.unsplash.com", "cdn.pixabay.com", "images.pexels.com",'i.pinimg.com', 'images.pexels.com','encrypted-tbn0.gstatic.com','www.lascrucesbulletin.com','5.imimg.com','i0.wp.com', 'www.bpmcdn.com','assets.architecturaldigest.in','www.thespruce.com','media.istockphoto.com']
   },
 };
 
